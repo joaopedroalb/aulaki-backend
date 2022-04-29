d8005d9d8dbc37855ee1baa1a27b196a20b8aa09
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-04-29 13:39:13 -0300

    feat: create class routes

--- src/routes/class.js
+++ src/routes/class.js
@@ -1,3 +1,29 @@
 const router = require('express').Router()
 
+router.get('/',async(req,res)=>{
+    const {title} = req.query
+
+    if(!title){
+        res.send({'result':'aqui vai todas as aulas'}) 
+    }else{
+        res.send({'result':`aqui vai as aulas filtradas com o titulo contendo ${title}`})
+    }
+})
+
+router.get('/search/:id', async(req,res)=>{
+    const {id} = req.params
+
+    res.send({'result':`aqui vai a aula com o id igual a ${id}`})
+})
+
+router.get('/teacher/:id', async(req,res)=>{
+    const {id} = req.params
+
+    res.send({'result':`aqui vai retornar as aulas do professor com id ${id}`})
+})
+
+router.get('/tag', async(req,res)=>{
+    res.send({'result':'essa rota vai retornar as tags mais usadas entre todas as aulas sendo no max 10 e no minimo 2'})
+})
+
 module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
 router.use('/teacher',require('./teacher'))
+router.use('/class',require('./class'))
 
 module.exports = router
