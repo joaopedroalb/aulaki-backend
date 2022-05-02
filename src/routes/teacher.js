978e39182c082a2b8f17f77bb9e2cceaeae80887
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-05-02 11:22:51 -0300

    feat: add controller functions in teacher routes

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -1,16 +1,19 @@
 const router = require('express').Router()
 
+const {getAll,getById,getCity} = require("../controller/teacher")
+
 router.get('/', async (req,res) =>{
-    const {name} = req.query
-    if(!name)
-        res.send({'result':'aqui vai retornar todos os teachers'})
-    else 
-        res.send({'result': `aqui vai retornar os teachers com os nomes ${name}`})
+    const {name,city} = req.query
+    res.send(getAll(name,city))
 })
 
-router.get('/:id', async (req,res) =>{
+router.get('/search/:id', async (req,res) =>{
     const {id} = req.params
-    res.send({'result':`aqui vai retornar os teachers com esse id : ${id}`})
+    res.send(getById(id))
+})
+
+router.get('/city',async(req,res)=>{
+    res.send(getCity())
 })
 
 module.exports = router
