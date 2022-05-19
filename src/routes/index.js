54c4871c3fef5c224e969a3c681eecb58c457a2b
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-05-19 17:31:40 -0300

    fix: fixing

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 
 router.use('/teacher',require('./teacher'))
 router.use('/class',require('./class'))
+router.get('/', async (req,res) =>{
+    return res.send({routes:"/teacher and /class"})
+})
 
 module.exports = router
